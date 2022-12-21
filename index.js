c24fe1e4f1dab73c2f608f73d18b5d63647b7ac2
Shakeeb Ahmad <shakes.ahmad@gmail.com>
2022-12-22 02:30:56 +0530

    replace duplicate var name

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write(`You are checking status for: ${websiteUrl}`)
     try {
-        http.get(websiteUrl, (res) => {
-            const statusCode = res.statusCode;
+        http.get(websiteUrl, (r) => {
+            const statusCode = r.statusCode;
             console.log({ statusCode });
             if (req.url == 'status') res.write("Status: " + statusCode);
             res.sendStatus(statusCode)
-            res.send(JSON.stringify({ statusCode }));
+            // res.send(statusCode );
         })
             .on("error", (error) => {
                 console.log({ statusCode: 500 });
                 res.sendStatus(500)
-                res.send(JSON.stringify({ statusCode: 500 }));
+                res.send(500);
             });
     } catch (error) {
         console.log(error);
